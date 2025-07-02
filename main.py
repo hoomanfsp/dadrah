a07401377eddd5ca4761fb6f72fe39d464243524
Hooman <hoomanfsp@gmail.com>
2025-07-02 10:16:16 +0330

    dockerize and its done

---
+++ main.py
@@ -0,0 +1,18 @@
+from dadrah_sc.crawler import extract_question_links, save_links_to_file
+from dadrah_sc.parser import parse_all_links
+
+def main():
+    try:
+        start = int(input("Start page number: "))
+        end = int(input("End page number: "))
+
+        links = extract_question_links(start, end)
+        save_links_to_file(links)
+
+        parse_all_links()
+
+    except Exception as e:
+        print(f"[!] Error: {e}")
+
+if __name__ == "__main__":
+    main()
